753e18e4bdc9be971f887e0fae19290dad51cd19
Murilo <murilonasoliveira@gmail.com>
2022-10-09 17:53:15 -0300

    add exercicio 6

---
+++ exercicio6.cpp
@@ -0,0 +1,18 @@
+//Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.;
+#include <stdio.h>
+#include <locale.h> 
+#include <conio.h>
+
+int main (){
+setlocale(LC_ALL,"portuguese");
+    float raio,A;
+        printf("Informe o raio de um circulo: ");
+            scanf("%f",&raio);
+    
+    
+    A = 3.14 * (raio * raio);
+
+printf("Área do circulo: %2.f m2",A);
+return(0);
+getch();
+}
